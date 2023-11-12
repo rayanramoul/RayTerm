5bd6c6d40e93a3e8b6e40a1e9e209b80af207d98
Rayan Samy RAMOUL <raysamram@gmail.com>
2023-11-12 02:22:16 +0100

    :bug: correct telescope bad mapping

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,5 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>pf", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>pf", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ps", function()
 builtin.grep_string({ search = vim.fn.input("Grep > ") })
