4e5077ab291b3a41be2d3d8c881c0b75c3750a9f
Rayan Samy Ramoul <raysamram@gmail.com>
2023-11-12 17:52:07 +0100

    :sprinkles: add py env visualization to lualine

--- lua/ray/init.lua
+++ lua/ray/init.lua
@@ -3,3 +3,23 @@ require("ray.set")
 -- print("Hello Ray")
 
 
+local virtual_env = function()
+  -- only show virtual env for Python
+  if vim.bo.filetype ~= 'python' then
+    return ""
+  end
+
+  local conda_env = os.getenv('CONDA_DEFAULT_ENV')
+  local venv_path = os.getenv('VIRTUAL_ENV')
+
+  if venv_path == nil then
+    if conda_env == nil then
+      return ""
+    else
+      return string.format("  %s (conda)", conda_env)
+    end
+  else
+    local venv_name = vim.fn.fnamemodify(venv_path, ':t')
+    return string.format("  %s (venv)", venv_name)
+  end
+end
