2e63e21d23b6088c545f9d3ebe812859d798bba8
Rayan Samy RAMOUL <raysamram@gmail.com>
2023-11-20 00:02:43 +0100

    :sprinkles: add configs related to tmux

--- setup_tmux.sh
+++ setup_tmux.sh
@@ -2,11 +2,13 @@
 # if not, clone it
 # copy tmux.conf to ~/.config/tmux/
 
-if [ ! -d "./tmux/plugins/tpm" ]; then
+if [ ! -d "~/.tmux/plugins/tpm" ]; then
     echo "Cloning tmux plugin manager"
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 tmux source tmux/tmux.conf
 cp -r tmux/ ~/.config/tmux/
 
 echo "Dont forget to install tmux plugin manager by running: "
 echo "ctrl b I"
+
