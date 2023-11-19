b14cb8d4c2d7c5b158cd859c6a62b98db25684db
Rayan Samy Ramoul <raysamram@gmail.com>
2023-11-19 19:27:30 +0100

    :sprinkles: replace barbar by buffalo and link it with harpoon

--- init.lua
+++ init.lua
@@ -30,6 +30,7 @@ plugins = {
     {'terrortylor/nvim-comment'},
     {'nvim-treesitter/nvim-treesitter-context'},
     {'akinsho/toggleterm.nvim'},
+    {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
     {
         'mfussenegger/nvim-dap-python',
         ft = {'python'},
@@ -120,20 +121,6 @@ plugins = {
     {'nvim-lualine/lualine.nvim'},
     {'kmontocam/nvim-conda'},
     {'gelguy/wilder.nvim'},
-    {'romgrk/barbar.nvim',
-    dependencies = {
-      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    },
-    init = function() vim.g.barbar_auto_setup = false end,
-    opts = {
-      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-      -- animation = true,
-      -- insert_at_start = true,
-      -- …etc.
-    },
-    version = '^1.0.0', -- optional: only update when a new 1.x version is released
-  },
     {
         "nvimdev/guard.nvim",
     -- Builtin configuration, optional
