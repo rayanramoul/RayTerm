606c403ff4e41d3c95900296a7cf79d4f5bebf86
Rayan Samy Ramoul <raysamram@gmail.com>
2023-11-29 17:58:09 +0100

    :sparkles: V2

--- setup_terminal.sh
+++ setup_terminal.sh
@@ -92,7 +92,7 @@ install_miniconda3() {
       # Download and install Miniconda
       wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
       chmod +x Miniconda3-latest-Linux-x86_64.sh
-      bash Miniconda3-latest-Linux-x86_64.sh
+      bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
 
       # Initialize Conda for the current shell
       source $HOME/miniconda3/etc/profile.d/conda.sh
