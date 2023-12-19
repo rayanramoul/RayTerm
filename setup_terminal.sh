cbca6e701607acddb4c9f0f057da3411ce3b2925
Rayan Samy RAMOUL <raysamram@gmail.com>
2023-12-19 22:10:05 +0100

    :bug: add fzf plugin cloning in setup terminal

--- setup_terminal.sh
+++ setup_terminal.sh
@@ -103,7 +103,7 @@ install_common_packages() {
   git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM}/themes/powerlevel10k"
-
+  git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
   echo "Downloading .zshrc and .p10k.zsh..."
   # Remove existing config files if they exist
   rm -f ~/.zshrc ~/.p10k.zsh
