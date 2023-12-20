4b3adecc6216764a194cf83ae5bbeb7d7f72fe9b
Rayan Samy RAMOUL <raysamram@gmail.com>
2023-12-20 15:06:26 +0100

    :bug: fix restoring of dotfiles

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,12 +1,42 @@
-rm -rf ~/.config-backup
-cp -r ~/.config/ ~/.config-backup
+#!/bin/bash
 
-# loop over all files and directories inside the dotfiles directory and ln -s them to the home directory
-# so that dotfiles would be as a home directory
+# Set the base directory
+base_dir="$(dirname "$(readlink -f "$0")")"
 
-DOTFILES_DIR=$(pwd)/dotfiles
-for file in $(ls -A $DOTFILES_DIR); do
-    if [ $file != "setup_dotfiles.sh" ]; then
-        ln -s $DOTFILES_DIR/$file ~/$file
+# Function to backup existing file or directory
+backup_and_link() {
+    local source_path=$1
+    local target_path=$2
+
+    # Backup existing file or directory
+    if [ -e "$target_path" ]; then
+        echo "Backing up existing $target_path"
+        mv "$target_path" "$target_path.bak"
+    fi
+
+    # Create symbolic link
+    ln -s "$source_path" "$target_path"
+    echo "Linked $source_path to $target_path"
+}
+
+# Loop through files in dotfiles directory
+for file in "$base_dir/dotfiles"/* "$base_dir/dotfiles"/.[^.]*; do
+    if [ -f "$file" ]; then
+        filename=$(basename "$file")
+        target_file="$HOME/$filename"
+        backup_and_link "$file" "$target_file"
     fi
 done
+
+# Loop through directories in .config directory inside dotfiles
+config_dir="$base_dir/dotfiles/.config"
+for dir in "$config_dir"/*; do
+    if [ -d "$dir" ]; then
+        dirname=$(basename "$dir")
+        target_dir="$HOME/.config/$dirname"
+        backup_and_link "$dir" "$target_dir"
+    fi
+done
+
+echo "Dotfiles setup completed successfully!"
+
