3dd255aa24170bc914abf09dd6242f419eb3c4c5
rayanramoul <raysamram@gmail.com>
2023-12-23 21:29:51 +0100

    :bug: experimetn

--- dotfiles/.config/nvim/lua/ray/remap.lua
+++ dotfiles/.config/nvim/lua/ray/remap.lua
@@ -18,13 +18,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
-
 -- greatest remap ever
 -- Allow to copy paste and dont have the buffer reinitialized after pasting
 vim.keymap.set("x", "<leader>p", [["_dP]])
