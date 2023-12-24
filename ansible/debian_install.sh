fe0072f7d28446cf81e526f8199ec638a7492227
rayanramoul <raysamram@gmail.com>
2023-12-24 15:19:07 +0100

    :bug:

--- ansible/debian_install.sh
+++ ansible/debian_install.sh
@@ -2,3 +2,5 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+
+snap install lsd    
