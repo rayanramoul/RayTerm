00f1bcbb36fd848c53de2a3e47f8f76ec645628e
Rayan Samy RAMOUL <raysamram@gmail.com>
2023-12-24 01:38:56 +0100

    :sparkles: update dockerfile declaration

---
+++ docker/build_test_docker.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+    echo "No arguments provided. Please specify 'arch', 'ubuntu', 'macos', or 'aws'."
+    exit 1
+fi
+
+# Set the base directory where your Dockerfiles are located
+BASE_DIR="."
+
+# Function to build and run Docker image
+build_and_run() {
+    docker build --no-cache -f "$BASE_DIR/$1.Dockerfile" -t "dotfiles-$1" .
+    docker run -it "dotfiles-$1"
+}
+
+# Case statement to handle different OS options
+case $1 in
+    arch)
+        build_and_run "arch"
+        ;;
+    ubuntu)
+        build_and_run "ubuntu"
+        ;;
+    macos)
+        build_and_run "macos"
+        ;;
+    aws)
+        build_and_run "aws"
+        ;;
+    *)
+        echo "Invalid argument. Please specify 'arch', 'ubuntu', 'macos', or 'aws'."
+        exit 1
+        ;;
+esac
+
