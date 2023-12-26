b4f1a59b7c2d1ee0d3efa55d7166ed2c7c49ec59
rayanramoul <raysamram@gmail.com>
2023-12-26 02:15:01 +0100

    :sparkles:

--- install.sh
+++ install.sh
@@ -1,112 +1,121 @@
 #!/bin/bash
 
-echo "---------------------------------------------------------"
-echo " /$$$$$$$                   /$$$$$$$$                              " 
-echo "| $$__  $$                 |__  $$__/                              " 
-echo "| $$  \ $$  /$$$$$$  /$$   /$$| $$  /$$$$$$   /$$$$$$  /$$$$$$/$$$$"
-echo "| $$$$$$$/ |____  $$| $$  | $$| $$ /$$__  $$ /$$__  $$| $$_  $$_  $$"
-echo "| $$__  $$  /$$$$$$$| $$  | $$| $$| $$$$$$$$| $$  \__/| $$ \ $$ \ $$"
-echo "| $$  \ $$ /$$__  $$| $$  | $$| $$| $$_____/| $$      | $$ | $$ | $$"
-echo "| $$  | $$|  $$$$$$$|  $$$$$$$| $$|  $$$$$$$| $$      | $$ | $$ | $$"
-echo "|__/  |__/ \_______/ \____  $$|__/ \_______/|__/      |__/ |__/ |__/"
-echo "                     /$$  | $$                                      "
-echo "                    |  $$$$$$/                                      "
-echo "                     \______/                                       "
-echo "---------------------------------------------------------"
+# Function to display colored ASCII art
+display_ascii_art() {
+  echo -e "\e[1;34m"
+  echo "---------------------------------------------------------"
+  echo " /$$$$$$$                   /$$$$$$$$                              "
+  echo "| $$__  $$                 |__  $$__/                              "
+  echo "| $$  \\ $$  /$$$$$$  /$$   /$$| $$  /$$$$$$   /$$$$$$  /$$$$$$/$$$$"
+  echo "| $$$$$$$/ |____  $$| $$  | $$| $$ /$$__  $$ /$$__  $$| $$_  $$_  $$"
+  echo "| $$__  $$  /$$$$$$$| $$  | $$| $$| $$$$$$$$| $$  \\__/| $$ \\ $$ \\ $$"
+  echo "| $$  \\ $$ /$$__  $$| $$  | $$| $$| $$_____/| $$      | $$ | $$ | $$"
+  echo "| $$  | $$|  $$$$$$$|  $$$$$$$| $$|  $$$$$$$| $$      | $$ | $$ | $$"
+  echo "|__/  |__/ \\_______/ \\____  $$|__/ \\_______/|__/      |__/ |__/ |__/"
+  echo "                     /$$  | $$                                      "
+  echo "                    |  $$$$$$/                                      "
+  echo "                     \\______/                                       "
+  echo "---------------------------------------------------------"
+  echo -e "\e[0m"
+}
+
+# Function to display colored messages
+display_message() {
+  echo -e "\e[1;32m$1\e[0m"
+}
+
+# Function to display colored error messages
+display_error() {
+  echo -e "\e[1;31m$1\e[0m"
+}
+
+# Function to display a prompt with color
+prompt_user() {
+  echo -e "\e[1;33m$1\e[0m"
+}
+
+# Display ASCII art
+display_ascii_art
+
 # Clone the dotfiles repository
 cd ~
-echo "Cloning dotfiles repository..."
+display_message "Cloning dotfiles repository..."
 git clone https://github.com/rayanramoul/RayTerm.git ~/RayTerm
 
 # Change into the cloned repository directory
 cd ~/RayTerm
 
-# Ask the user if they want to install all packages and set up dotfiles
-
-# Function to install Ansible on Debian-based systems (e.g., Ubuntu)
-install_ansible_debian() {
-    sudo apt update
-    sudo apt install -y ansible
-}
-
-# Function to install Ansible on Red Hat-based systems (e.g., Amazon Linux)
-install_ansible_redhat() {
-    sudo yum install -y ansible
-}
-
-# Function to install Ansible on Arch Linux
-install_ansible_arch() {
-    sudo pacman -Sy --noconfirm ansible
-}
-
-# Function to install Ansible on macOS using Homebrew
-install_ansible_macos() {
-    brew install ansible
+# Function to install Ansible on different platforms
+install_ansible() {
+  case "$1" in
+    debian|ubuntu)
+      sudo apt update
+      sudo apt install -y ansible
+      ;;
+    centos|rhel|fedora|amazon)
+      sudo yum install -y ansible
+      ;;
+    arch|manjaro)
+      sudo pacman -Sy --noconfirm ansible
+      ;;
+    darwin)
+      brew install ansible
+      ;;
+    *)
+      display_error "Unsupported platform: $1"
+      exit 1
+      ;;
+  esac
 }
 
 # Determine the OS
-
 OS=$(uname -a)
 
-# Check if "Microsoft" is present in the uname output, indicating WSL
+# Check if running on WSL
 if echo "$OS" | grep -qi "microsoft"; then
-    # Determine the specific WSL distribution
-    WSL_DISTRO=$(wsl.exe -l -q | head -n 1)
+  # Determine the specific WSL distribution
+  WSL_DISTRO=$(wsl.exe -l -q | head -n 1)
 
-    case "$WSL_DISTRO" in
-        Ubuntu*)
-            install_ansible_debian
-
-            ;;
-        Arch*)
-            install_ansible_arch
-            ;;
-        *)
-            echo "Unsupported WSL distribution: $WSL_DISTRO"
-            exit 1
-
-            ;;
-    esac
+  case "$WSL_DISTRO" in
+    Ubuntu*)
+      install_ansible ubuntu
+      ;;
+    Arch*)
+      install_ansible arch
+      ;;
+    *)
+      display_error "Unsupported WSL distribution: $WSL_DISTRO"
+      exit 1
+      ;;
+  esac
 else
-    # Install Ansible based on the detected OS
-    case "$OS" in
-        Linux*)
-            if [ -f /etc/os-release ]; then
-                source /etc/os-release
-                case "$ID" in
-                    debian|ubuntu)
-                        install_ansible_debian
-                        ;;
-                    centos|rhel|fedora|amazon)
-                        install_ansible_redhat
-                        ;;
-                    # if arch or manjaro
-                    arch|manjaro)
-                        install_ansible_arch
-                        ;;
-                    *)
-                        echo "Unsupported Linux distribution: $ID"
-                        exit 1
-                        ;;
-
-                esac
-            else
-                echo "Unable to determine Linux distribution."
-                exit 1
-
-            fi
-            ;;
-        Darwin*)
-
-            install_ansible_macos
-            ;;
-        *)
-            echo "Unsupported operating system: $OS"
-            exit 1
-            ;;
-    esac
+  # Install Ansible based on the detected OS
+  case "$OS" in
+    Linux*)
+      if [ -f /etc/os-release ]; then
+        source /etc/os-release
+        display_message "Installing Ansible on $ID..."
+        install_ansible "$ID"
+      else
+        display_error "Unable to determine Linux distribution."
+        exit 1
+      fi
+      ;;
+    Darwin*)
+      display_message "Installing Ansible on macOS..."
+      install_ansible darwin
+      ;;
+    *)
+      display_error "Unsupported operating system: $OS"
+      exit 1
+      ;;
+  esac
 fi
 
 # Execute the Ansible playbook
+prompt_user "Press Enter to execute the Ansible playbook..."
+display_message "Executing Ansible playbook..."
 ansible-playbook --ask-become-pass -i ansible/inventory.ini ansible/setup_terminal.yml
+
+display_message "Script execution completed successfully!"
 
