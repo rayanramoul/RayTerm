26ead5d82efe07f63c78ab1de4ca341b769cad63
r.ramoul <r.ramoul@instadeep.com>
2024-03-10 20:33:31 +0100

    feat: update lsp

--- dotfiles/.config/nvim/after/plugin/lsp.lua
+++ dotfiles/.config/nvim/after/plugin/lsp.lua
@@ -200,6 +200,7 @@ lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
