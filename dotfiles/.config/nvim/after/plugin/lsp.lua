5de6663e689c545ba29f1618161e11a7510ffb3a
r.ramoul <r.ramoul@instadeep.com>
2024-03-15 00:38:01 +0100

    feat: make lsp find references use telescope for display

--- dotfiles/.config/nvim/after/plugin/lsp.lua
+++ dotfiles/.config/nvim/after/plugin/lsp.lua
@@ -228,7 +228,10 @@ lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+  -- vim.keymap.set("n", "gr", "<cmd>Telescope lsp_definitions<cr>", opts)
+  -- vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+  -- feed lsp.buf.references() to telescope
+  vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
