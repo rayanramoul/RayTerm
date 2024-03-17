458b4e98290a890bb9f9ce82eee375b6d8d61704
Ray <raysamram@gmail.com>
2024-03-17 20:18:47 +0100

    Update lazy.lua

--- dotfiles/.config/nvim/lua/ray/lazy.lua
+++ dotfiles/.config/nvim/lua/ray/lazy.lua
@@ -307,9 +307,9 @@ return {
 
             local opts = { noremap = true, silent = true }
 
-            vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", opts)
-            vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
-            vim.keymap.set("i", "::", "<cmd>IconPickerInsert<cr>", opts)
+           -- vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", opts)
+           -- vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
+           -- vim.keymap.set("i", "::", "<cmd>IconPickerInsert<cr>", opts)
         end
     },
     {'edluffy/hologram.nvim', { }},
