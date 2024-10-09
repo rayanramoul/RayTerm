eab647be62f5a2ba45a9f6e2a0d84d55d59dd64f
rayanramoul <raysamram@gmail.com>
2024-10-09 14:13:17 +0200

    feat: prevent dapui from closing automatically

---
+++ dotfiles/.config/nvim/lua/plugins/dapui.lua
@@ -0,0 +1,11 @@
+return {
+  "rcarriga/nvim-dap-ui",
+  config = function(_, opts)
+    local dap = require("dap")
+    local dapui = require("dapui")
+    dapui.setup(opts)
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open({})
+    end
+  end,
+}
