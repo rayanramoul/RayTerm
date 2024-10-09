705014435c29e2a69cc5d1f2e4a1188a3bb7a0a5
rayanramoul <raysamram@gmail.com>
2024-10-09 19:58:24 +0200

    feat: add transparency to neovim and kitty

--- dotfiles/.config/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,21 @@
 return {
-  -- add gruvbox
-  -- { "rose-pine/neovim" },
-  -- { "projekt0n/github-nvim-theme" },
-  -- { "tiagovla/tokyodark.nvim" },
-  { "yorumicolors/yorumi.nvim" },
-
-  -- Configure LazyVim to load gruvbox
+  -- add rose-pine
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "auto",
+      dark_variant = "main",
+      styles = {
+        transparency = true,
+      },
+    },
+  },
+  -- Configure LazyVim to load rose-pine
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "rose-pine",
-      -- colorscheme = "github_dark_default",
-      -- colorscheme = "tokyodark",
-      colorscheme = "yorumi",
+      colorscheme = "rose-pine",
     },
   },
 }
