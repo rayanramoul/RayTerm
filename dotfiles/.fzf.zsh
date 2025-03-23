208ae08b6b79ea53ce25a5c8886aabcc7f9fe2a3
rayanramoul <raysamram@gmail.com>
2025-03-23 18:23:31 +0100

    fix: update .fzf sourced files

--- dotfiles/.fzf.zsh
+++ dotfiles/.fzf.zsh
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
+if [[ ! "$PATH" == */Users/r.ramoul/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/$HOME/.fzf/bin"
 fi
 
 # Auto-completion
